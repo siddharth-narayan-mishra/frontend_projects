e8364e90713282004cb841023f75e82ad0893b73
siddharth-narayan-mishra <siddharthnmishra7@gmail.com>
2023-12-19 17:31:36 +0530

    deleted test files

--- weather/test.js
+++
@@ -1,5 +0,0 @@
-fetch('http://api.weatherapi.com/v1/forecast.json?key=399cf94427c941d6afd45707230712&q=London&days=5&aqi=no&alerts=no')
-.then((response)=>response.json())
-.then((data)=>{
-    console.log(data);
-})
